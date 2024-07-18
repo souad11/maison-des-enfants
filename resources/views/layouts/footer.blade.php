daff4ae1b388e757a52f3087444a469fdac98cf3
souad <riouch.souad@gmail.com>
2024-07-18 15:23:51 +0200

    Integration template

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,46 @@
+<footer>
+<!-- Footer Start -->
+ <div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
+            <div class="container py-5">
+                <div class="row g-5">
+                    
+                    <div class="col-lg-3 col-md-6">
+                        <h3 class="text-white mb-4">Informations</h3>
+                        <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>17 Rue Dethy, 1060 Saint-Gilles</p>
+                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+32 534 26 26</p>
+                        <p class="mb-2"><i class="fa fa-envelope me-3"></i>mde@asbl.com</p>
+                        <div class="d-flex pt-2">
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <h3 class="text-white mb-4">Liens rapides</h3>
+                        <a class="btn btn-link text-white-50" href="">A propos</a>
+                        <a class="btn btn-link text-white-50" href="">Contact</a>
+                        <a class="btn btn-link text-white-50" href="">Nos activités</a>
+                        <a class="btn btn-link text-white-50" href="">Privacy Policy</a>
+                        <a class="btn btn-link text-white-50" href="">Terms & Condition</a>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                    <h3 class="text-white mb-4">Heures d'ouverture</h3>
+                    <div class="item">
+                        <ul class="list-unstyled">
+                            <li class="d-flex justify-content-between"><span>Lu - Ma</span><span>08:00 - 18:00</span></li>
+                            <li class="d-flex justify-content-between"><span>Mer - Ven</span><span>08:00 - 18:00</span></li>
+                            <li class="d-flex justify-content-between"><span>Samedi</span><span>13:30 - 17:00</span></li>
+                            <li class="d-flex justify-content-between"><span>Dimanche</span><span class="Closed">Fermé</span></li>
+                        </ul>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="footer_bottom bg-dark text-white-50">
+        <div class="container text-center py-3">
+            <p class="mb-0">Copyright &copy; MDE 2000. All Rights Reserved</p>
+        </div>
+    </div>
+        <!-- Footer End -->
+</footer>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,60 @@
+<header>
+
+      
+
+
+        <!-- Navbar Start -->
+        <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top px-4 px-lg-5 py-lg-0">
+            <a href="index.html" class="navbar-brand">
+            <img src="logo/logo.svg" alt="Logo" style="width: 150px; height: auto;">
+            </a>
+            
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav mx-auto">
+                    <a href="#" class="nav-item nav-link active">Accueil</a>
+                    <a href="#" class="nav-item nav-link">A propos</a>
+                    <a href="#" class="nav-item nav-link">Nos partenaires</a>
+                    <a href="#" class="nav-item nav-link">Notre équipe</a>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Nos activités</a>
+                        <div class="dropdown-menu rounded-0 rounded-bottom border-0 shadow-sm m-0">
+                            <a href="#" class="dropdown-item">Activités à l'année</a>
+                            <a href="#" class="dropdown-item">Activités hebdomadaires</a>
+                        </div>
+                    </div>
+                    <a href="contact.html" class="nav-item nav-link">Contact</a>
+                </div>
+                <div class="header_top_right">
+                <ul class="list-inline">
+                    @guest
+                    <li class="list-inline-item">
+                        <a href="{{ route('register') }}" class="btn btn-primary rounded-pill px-3 d-none d-lg-block">S'inscrire</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="{{ route('login') }}" class="btn btn-primary rounded-pill px-3 d-none d-lg-block">Se connecter</a>
+                    </li>
+                    @else
+                    <li class="list-inline-item" style="margin-right: 20px;">
+                        <a class="btn btn-primary rounded-pill px-3 d-none d-lg-block" href="{{ url('/my-dashboard') }}">Mon tableau de bord</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="btn btn-primary rounded-pill px-3 d-none d-lg-block" href="{{ route('custom-logout') }}"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            <i class="fas fa-sign-out-alt"></i> Se déconnecter
+                        </a>
+                        <form id="logout-form" action="{{ route('custom-logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </li>
+                    @endguest
+                </ul>
+            </div>
+
+
+            </div>
+        </nav>
+        <!-- Navbar End -->
+</header>
